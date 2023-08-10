06603ed8078d9fcfe8885d415291905c44e6ab78
prashantkannaujiya <prashantkannaujiya@gmail.com>
2023-08-10 20:46:57 +0530

    change the way to connect

--- talentbox.js
+++ talentbox.js
@@ -2,9 +2,9 @@ var express=require('express');
 var app=express();
 var bodyParser=require('body-parser');
 var cors=require('cors');
-var MongoClient=require('mongodb').MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var jwt=require('jsonwebtoken')
-var url=process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/";  
+var uri='mongodb+srv://Prashant_Kannaujiya:Rajan$9935@cluster0.aweosln.mongodb.net/?retryWrites=true&w=majority';
 
 app.set('views','./views');
 app.use(cors());
@@ -12,11 +12,47 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 var db;
-MongoClient.connect(url,async (err,con)=>{
+const client = new MongoClient(uri);
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      
+      // Send a ping to confirm a successful connection
+      db=client.db("talent");
+     // 
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      var k=await db.collection('test').insertOne({owner:'Prashant'});
+      console.log(k)
+    } finally {
+      // Ensures that the client will close when you finish/error
+      //await client.close();
+      var p=await db.collection('test').findOne({'courses.courseName':'Complete Java'});
+      console.log(p)
+      var c=[
+       {courseName:'Complete Java',pic:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdev.java%2F&psig=AOvVaw2C-XftxPM13sJoEx2uP_mI&ust=1690609069896000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjow9rXsIADFQAAAAAdAAAAABAE'},
+       {courseName:'SQl-Begineers Guide',pic:'https://seeklogo.com/images/A/azure-sql-managed-instance-logo-D33A77DD5E-seeklogo.com.png'},
+       {courseName:'Learn MongoDB from scratch',pic:'https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png'},
+       {courseName:'Backend development',pic:'https://cdn-icons-png.flaticon.com/512/8010/8010472.png'},
+       {courseName:'Data Visualization',pic:'https://w7.pngwing.com/pngs/997/670/png-transparent-data-analysis-analytics-data-visualization-information-business-people-business-data-thumbnail.png'}
+   ]
+   if(p==null)
+   {
+       db.collection('test').insertOne({courses:c}).then((data)=>{console.log(data)}).catch(err=>console.log(err))
+   }
+    
+       
+    }
+  }
+  run().catch(console.dir);
+  
+ /*
+MongoClient.connect(uri,async (err,con)=>{
     if(err){console.log(err)}
     else{
    db=con.db('talent');
+
    var p=await db.collection('test').findOne({courseName:'Complete Java'})
+   console.log(p)
    var c=[
     {courseName:'Complete Java',pic:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdev.java%2F&psig=AOvVaw2C-XftxPM13sJoEx2uP_mI&ust=1690609069896000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjow9rXsIADFQAAAAAdAAAAABAE'},
     {courseName:'SQl-Begineers Guide',pic:'https://seeklogo.com/images/A/azure-sql-managed-instance-logo-D33A77DD5E-seeklogo.com.png'},
@@ -30,7 +66,7 @@ if(p==null)
 }
  
     }
-})
+})*/
 app.post('/insertGoogleData',async(req,res)=>{
     console.log(req.body.email)
     db.collection('box').findOne({email:req.body.email})
